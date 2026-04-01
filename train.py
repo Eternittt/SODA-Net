99caebbfea459fd0cc17f890d3016009533661f4
Eternittt <136587748+Eternittt@users.noreply.github.com>
2026-04-02 01:14:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch
 
 
 if __name__ == '__main__':
-    model = YOLO(r'ultralytics/cfg/models/SODA-YOLO/SODA-YOLO.yaml')
+    model = YOLO(r'ultralytics/cfg/models/SODA-Net/SODA-Net.yaml')
     torch.cuda.empty_cache()
     model.train(data='dataset.yaml',
                 imgsz=640,
